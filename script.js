1138e22b69730d09fc7eab702d70b9b2743efc61
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-03-30 18:27:55 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+firstLetterOfLastName = lastName.length;
+console.log(firstLetterOfLastName);
+console.log(lastName);
+*/
+
+let firstLetterOfHelloWorld = "";
+let myStr = "Jello World";
+firstLetterOfHelloWorld = myStr[0];
+firstLetterOfHelloWorld -= myStr;
+console.log(firstLetterOfHelloWorld);
