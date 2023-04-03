f77386eb97cb7e2fea75350f25806c82c5204dc1
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-04-03 18:37:26 +0800

    update my training

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/*let firstLetterOfLastName = "";
+/*
+let firstLetterOfLastName = "";
 const lastName = "Lovelace";
 firstLetterOfLastName = lastName[0];
 firstLetterOfLastName = lastName.length;
@@ -6,8 +7,34 @@ console.log(firstLetterOfLastName);
 console.log(lastName);
 */
 
+/*
 let firstLetterOfHelloWorld = "";
 let myStr = "Jello World";
 firstLetterOfHelloWorld = myStr[0];
 firstLetterOfHelloWorld -= myStr;
 console.log(firstLetterOfHelloWorld);
+*/
+
+/*
+const lastName = "dela cruz";
+
+const fourthLetterOfMyLastName = lastName[5];
+console.log(fourthLetterOfMyLastName);
+
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks =
+  "It was really a + "myAdjective" + "myNoun" and we + "myVerb" + "myAdverb";
+console.log(wordBlanks);
+
+*/
+
+const name = "john";
+const age = 35;
+
+const message = `My name is ${name} and my age is ${age} years old.`;
+console.log(message);
