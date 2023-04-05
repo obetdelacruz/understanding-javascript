635847f982da7068490709d35088576de1ad268c
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-04-05 17:51:31 +0800

    practice again in funciton

--- script.js
+++ script.js
@@ -31,10 +31,30 @@ const wordBlanks =
   "It was really a + "myAdjective" + "myNoun" and we + "myVerb" + "myAdverb";
 console.log(wordBlanks);
 
-*/
+
 
 const name = "john";
 const age = 35;
 
 const message = `My name is ${name} and my age is ${age} years old.`;
 console.log(message);
+
+
+let ten = 10;
+console.log(ten * ten);
+*/
+
+// food prices
+let beerCost = 10;
+let burgerCost = 15;
+let softdrinkCost = 15;
+
+// user's money
+let accountBalance = 30;
+
+function accountBalance() {
+  accountBalance = accountBalance - beerCost - burgerCost - softdrinkCost;
+  console.log(accountBalance);
+}
+
+drinkBeer(accountBalance);
